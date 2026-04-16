268ea8c5fb061cfea45306a9dddd236a8750477f
marinagarciaguado <marinagarciaguado@gmail.com>
2026-04-16 19:52:58 +0200

    correcion creacion de codigo en gestion_produccion/models.py

--- gestion_produccion/models.py
+++ gestion_produccion/models.py
@@ -1,3 +1,4 @@
+import uuid # Añade esta importación al principio
 from django.db import models
 from gestion_materia_prima.models import LoteMateriaPrima
 
@@ -11,8 +12,15 @@ class LoteProduccion(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=15, choices=ESTADOS, default='ARMADO')
     
-    # Un lote de producción agrupa varios de materia prima 
     lotes_materia_prima = models.ManyToManyField(LoteMateriaPrima)
+
+    # --- AÑADE ESTO ---
+    def save(self, *args, **kwargs):
+        if not self.codigo_seguimiento:
+            # Genera un código único corto, ej: PROD-1A2B3C
+            self.codigo_seguimiento = f"PROD-{uuid.uuid4().hex[:8].upper()}"
+        super().save(*args, **kwargs)
+    # ------------------
 
     def __str__(self):
         return f"Producción #{self.codigo_seguimiento} ({self.estado})"
